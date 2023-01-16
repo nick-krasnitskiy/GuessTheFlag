412a834faa1d92e0489f0979a55c98b0e85aa2a4
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-01-16 13:26:48 +0300

    Add the desert alert.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var showingFinal = false
+    
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var userScore = 0
+    @State private var questionCount = 0
     
     var body: some View {
         ZStack {
@@ -39,6 +43,7 @@ struct ContentView: View {
                     
                     ForEach(0..<3) { number in
                         Button {
+                            questionCount += 1
                             flagTapped(number)
                         } label: {
                             Image(countries[number])
@@ -56,7 +61,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -67,23 +72,44 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(userScore)")
+        }
+        
+        .alert(scoreTitle, isPresented: $showingFinal) {
+            Button("Reset", action: reset)
+        } message: {
+            Text("Reset the game!")
         }
     }
     
     func flagTapped(_ number: Int) {
+        
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            scoreTitle = "Correct!"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])!"
+            userScore -= 1
         }
         
-        showingScore = true
+        if questionCount <= 8 {
+            showingScore = true
+        } else {
+            scoreTitle = "Game over!"
+            showingFinal = true
+        }
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        userScore = 0
+        questionCount = 0
+        askQuestion()
+        showingFinal = false
     }
 }
 
