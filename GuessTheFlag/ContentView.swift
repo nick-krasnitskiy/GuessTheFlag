71a7d0ab85ce24fab64a1eb6137e8085009d684d
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-01-18 12:19:18 +0300

    Add a reset game logic.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,9 +11,11 @@ struct ContentView: View {
     @State private var contries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
+    @State private var showingFinalAlert = false
     @State private var scoreTitle = ""
     @State private var scoreMessage = ""
     @State private var userScore: Int = 0
+    @State private var numberOFQuestions: Int = 0
     
     var body: some View {
         ZStack {
@@ -21,7 +23,7 @@ struct ContentView: View {
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack {
                 Spacer()
@@ -69,9 +71,16 @@ struct ContentView: View {
         } message: {
             Text(scoreMessage)
         }
+        .alert("Congtatulation!", isPresented: $showingFinalAlert) {
+            Button("Reset the game", action: reset)
+        } message: {
+            Text("Your final score is \(userScore)")
+        }
     }
     
     func flagTapped(_ number: Int) {
+        numberOFQuestions += 1
+        
         if number == correctAnswer {
             userScore += 1
             scoreTitle = "Correct"
@@ -82,10 +91,22 @@ struct ContentView: View {
             scoreMessage = "That’s the flag of \(contries[number]). Your score is \(userScore)"
         }
         
-        showingScore = true
+        if numberOFQuestions < 8 {
+            showingScore = true
+        } else {
+            showingFinalAlert = true
+        }
+        
     }
     
     func askQuestion() {
+        contries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        numberOFQuestions = 0
+        userScore = 0
         contries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
