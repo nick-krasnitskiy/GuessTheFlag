a0561ba52aae20b7b15e564b14667ad91833c025
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-05-03 16:11:40 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nick Krasnitskiy on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
